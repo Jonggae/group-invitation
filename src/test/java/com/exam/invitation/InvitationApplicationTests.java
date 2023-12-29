9730c94b869ecacf6fcdaf3308d411f7ec43348f
Jongwoo Choi <muvnelik@naver.com>
2023-12-29 16:10:35 +0900

    1차 리팩토링 - 테스트코드
    리팩토링 후 테스트코드가 잘 작동하지 않음. 어떤 방식으로 짜야 테스트코드를 완벽하게 구동할 수 있는가

--- src/test/java/com/exam/invitation/InvitationApplicationTests.java
+++ src/test/java/com/exam/invitation/InvitationApplicationTests.java
@@ -11,42 +11,42 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InvitationApplicationTests {
-    // 테스트 코드 작성 시, 선행되어야 할 조건들과 그 이유는 무엇일까? ↓↓
-    // repository를 임포트, Autowired의 사용 이유
-
-    private final MemberRepository memberRepository;
-    private final InvitationLinkRepository invitationLinkRepository;
-
-    @Autowired
-    public InvitationApplicationTests(MemberRepository memberRepository, InvitationLinkRepository invitationLinkRepository) {
-        this.memberRepository = memberRepository;
-        this.invitationLinkRepository = invitationLinkRepository;
-    }
-
-    @Test
-    void 임시회원생성() {
-        // 회원 저장
-        Member member = new Member();
-        member.setName("CJW");
-        member.setEmail("abc@mail.com");
-        member.setPhoneNumber("0001234567");
-        memberRepository.save(member);
-
-        //임시 회원 검색
-        Member findMember = memberRepository.findById(member.getId()).get(); // .get();을 왜 쓰는지?
-        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
-    }
-
-    @Test
-    void 초대링크생성() {
-
-        InvitationLink link = new InvitationLink();
-        link.setLink("asdfasdf");
-        invitationLinkRepository.save(link);
-
-        InvitationLink findLink = invitationLinkRepository.findById(link.getId()).get();
-        Assertions.assertThat(link.getLink()).isEqualTo(findLink.getLink());
-
-    }
+//    // 테스트 코드 작성 시, 선행되어야 할 조건들과 그 이유는 무엇일까? ↓↓
+//    // repository를 임포트, Autowired의 사용 이유
+//
+//    private final MemberRepository memberRepository;
+//    private final InvitationLinkRepository invitationLinkRepository;
+//
+//    @Autowired
+//    public InvitationApplicationTests(MemberRepository memberRepository, InvitationLinkRepository invitationLinkRepository) {
+//        this.memberRepository = memberRepository;
+//        this.invitationLinkRepository = invitationLinkRepository;
+//    }
+//
+//    @Test
+//    void 임시회원생성() {
+//        // 회원 저장
+//        Member member = new Member();
+//        member.setName("CJW");
+//        member.setEmail("abc@mail.com");
+//        member.setPhoneNumber("0001234567");
+//        memberRepository.save(member);
+//
+//        //임시 회원 검색
+//        Member findMember = memberRepository.findById(member.getId()).get(); // .get();을 왜 쓰는지?
+//        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+//    }
+//
+//    @Test
+//    void 초대링크생성() {
+//
+//        InvitationLink link = new InvitationLink();
+//        link.setLink("asdfasdf");
+//        invitationLinkRepository.save(link);
+//
+//        InvitationLink findLink = invitationLinkRepository.findById(link.getId()).get();
+//        Assertions.assertThat(link.getLink()).isEqualTo(findLink.getLink());
+//
+//    }
 
 }
--- src/test/java/com/exam/invitation/service/TempMemberServiceTest.java
+++ src/test/java/com/exam/invitation/service/TempMemberServiceTest.java
@@ -1,71 +1,47 @@
 package com.exam.invitation.service;
 
-import com.exam.invitation.domain.Member;
 import com.exam.invitation.dto.MemberDto;
 import com.exam.invitation.repository.MemberRepository;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Transactional
 class TempMemberServiceTest {
+    private final MockMvc mvc;
+
+    public TempMemberServiceTest(MockMvc mvc) {
+        this.mvc = mvc;
+    }
 
     @Autowired
-    private MemberService tempMemberService;
+    private MemberService memberService;
 
-    // autowired가 없으면 오류가 발생
-    // Cannot invoke "com.exam.invitation.service.MemberService.createTempMember(String, String, String)" because "this.tempMemberService" is null
-    @Autowired private MemberRepository tempMemberRepository;
-
-    @Test
-    @Disabled
-    @DisplayName("임시 회원 생성되는지 확인 -> 목적이 다른 테스트 코드로 비활성화함")
-    void createTempMember() {
-        // given
-        Member tempMember = new Member();
-
-        // when
-        tempMember.setName("CJW");
-        tempMember.setEmail("CJW@mail.com");
-        tempMember.setPhoneNumber("01012345678");
-
-        // then
-        assertThat(tempMember)
-                .isNotNull()
-                .hasFieldOrPropertyWithValue("name","CJW")
-                .hasFieldOrPropertyWithValue("email","CJW@mail.com")
-                .hasFieldOrPropertyWithValue("phoneNumber","01012345678");
-        // 이상의 테스트는 메서드를 테스트하는 것이 아니라 다시 작성하기로 함
-    }
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     @DisplayName("임시 회원 생성되는지 확인")
     @Transactional
-    void createTempMember2() {
+    void createTempMember() {
         //given
-        MemberDto tempMemberDto = new MemberDto();
-        String name = "CJW";
-        String email = "CJW@mail.com";
-        String phoneNumber = "01012345678";
 
         //when
-        tempMemberService.createTempMember(tempMemberDto);
 
         // then
-        Optional<Member> savedTempMember = tempMemberRepository.findByName(tempMemberDto.getName());
 
-        assertThat(savedTempMember).isNotNull();
-        assertThat(savedTempMember.get().getName()).isEqualTo(tempMemberDto.getName());
-        assertThat(savedTempMember.get().getEmail()).isEqualTo(tempMemberDto.getEmail());
-        assertThat(savedTempMember.get().getPhoneNumber()).isEqualTo(tempMemberDto.getPhoneNumber());
-        assertThat(savedTempMember.get().isActivated()).isFalse();
+
+    }
+
+    private MemberDto createMemberDto() {
+        return MemberDto.of(
+                "CJW",
+                "CJW@mail.com",
+                "01012345678"
+        );
     }
 }
